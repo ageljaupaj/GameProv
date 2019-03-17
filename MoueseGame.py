8f9a3d041b9b825916faae94d37312b4c282fd51
ageljaupaj <ageljaupaj97@gmail.com>
2019-03-17 18:07:44 +0100

    I shtova slowmotion

--- MoueseGame.py
+++ MoueseGame.py
@@ -1,28 +1,31 @@
 import pygame
 background_colour = (0,  0, 0)
-(width, height) = (300, 300)
+(width, height) = (1000, 1000)
 
 x = 150
 y = 150
-speed = 0.01
+speed = 0.3
 
 character = pygame.image.load('Sprites/MouseCharacter.png')
 imagerect = character.get_rect()
 
 
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('kristo idioti')
-
-print('hello')
+pygame.display.set_caption('kot')
 
 
-running = Trues
+running = True
 while running:
   screen.fill(background_colour)
   screen.blit(character, imagerect)
   pygame.display.flip()
   imagerect.center = (x, y)
-  keys = pygame.key.get_pressed()
+  keys = pygame.key.get_pressed() 
+  
+  if keys[pygame.K_SPACE]:
+    speed = 0.1
+  if keys[pygame.K_SPACE] == False:
+    speed = 0.3
   
   if keys[pygame.K_w]:
     x = x + 0
@@ -37,12 +40,13 @@ while running:
     x = x + speed
     y = y + 0
   
-
-  
   
   for event in pygame.event.get():
-    
-      
+#    if event.type == pygame.KEYDOWN:
+#            if event.key == pygame.K_SPACE:
+#              speed == 0.1
+#            else:
+#              speed == 0.3
 
     if event.type == pygame.QUIT:
       running = False
